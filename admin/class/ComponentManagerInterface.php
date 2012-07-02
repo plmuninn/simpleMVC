5f094df5525ad045e4f1e4117e127d070b9e6ae8
Maciej <biuro@onewebpro.pl>
2012-07-02 19:43:35 +0200

    Added classes

---
+++ admin/class/ComponentManagerInterface.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: loki
+ * Date: 02.07.12
+ * Time: 19:41
+ *
+ */
+interface ComponentManagerInterface
+{
+
+}
---
+++ admin/class/PluginManager.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: loki
+ * Date: 02.07.12
+ * Time: 19:39
+ *
+ */
+class PluginManager implements PluginManagerIterface
+{
+
+}
