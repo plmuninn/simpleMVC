5de0ac0b176d5db531f19bb6b954bff225646509
Maciej <biuro@onewebpro.pl>
2012-07-02 19:38:38 +0200

    Change config.php

--- config.php
+++ config.php
@@ -9,17 +9,17 @@
 
 
 /*Site configuration*/
-define("SITE_PATH","homepage");
+define("SITE_PATH","simpleMVC");
 define("BASE_DIR", dirname(__FILE__) . DIRECTORY_SEPARATOR);
 define("SITE_TITLE","Radio Forum");
 define("SITE_CHARSET", "utf-8");
 
 /*Database configuration*/
 define("DB_TYPE","mysql");
-define("DB_NAME" ,"loki_mvc");
-define("DB_USER", "loki_loki");
-define("DB_PASSWORD","zielone");
-define("DB_ADDRES","localhost");
+define("DB_NAME" ,"");
+define("DB_USER", "");
+define("DB_PASSWORD","");
+define("DB_ADDRES","");
 define("DB_CHARSET", "utf8");
 
 
