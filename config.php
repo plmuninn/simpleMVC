ea415376a517fc5cc9c7b9c5b4d822f7b3632597
Maciej <biuro@onewebpro.pl>
2012-07-19 08:25:32 +0200

    Config

--- config.php
+++ config.php
@@ -15,19 +15,12 @@ define("SITE_TITLE","Radio Forum");
 define("SITE_CHARSET", "utf-8");
 
 /*Database configuration*/
-define("DB_TYPE","mysql");
-define("DB_NAME" ,"test");
-define("DB_USER", "root");
-define("DB_PASSWORD","zielone");
-define("DB_ADDRES","localhost");
+define("DB_TYPE","");
+define("DB_NAME" ,"");
+define("DB_USER", "");
+define("DB_PASSWORD","");
+define("DB_ADDRES","");
 define("DB_CHARSET", "utf8");
 
-/*
-define("DB_TYPE","mysql");
-define("DB_NAME" ,"loki_mvc");
-define("DB_USER", "loki_loki");
-define("DB_PASSWORD","zielone");
-define("DB_ADDRES","localhost");
-define("DB_CHARSET", "utf8");
-*/
+
 
